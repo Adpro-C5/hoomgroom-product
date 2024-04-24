d52760f9bb03e4cab11084dffe46840b3befef60
astrialyanda <alyandaastri19@gmail.com>
2024-04-24 20:33:28 +0700

    [RED] add Catalogue Repository skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/product/repository/CatalogueRepository.java
@@ -0,0 +1,18 @@
+package id.ac.ui.cs.advprog.product.repository;
+
+import id.ac.ui.cs.advprog.product.model.Product;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class CatalogueRepository {
+    private List<Product> productData;
+
+    public Iterator<Product> findAll() {
+        return null;
+    }
+
+    public Product findById(String id) {
+        return null;
+    }
+}
