93ac1ad8db8ca4d5094a4b34e7325e6a3a0faae0
astrialyanda <alyandaastri19@gmail.com>
2024-05-01 21:07:33 +0700

    [RED] catalogue controller skeleton

--- src/main/java/id/ac/ui/cs/advprog/product/controller/CatalogueController.java
+++ src/main/java/id/ac/ui/cs/advprog/product/controller/CatalogueController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CatalogueController {
 
-    @GetMapping("/catalogue")
+    @GetMapping("/product/catalogue")
     public String productCataloguePage() {
         return "Catalogue";
     }
+
+    @GetMapping("/product/details/")
+    public String productDetailPage() { return "Details"; }
 }
