237589f90416bc707994b70645a263448b854ee2
astrialyanda <alyandaastri19@gmail.com>
2024-05-01 21:06:30 +0700

    [GREEN] catalogue service findAll method

--- src/main/java/id/ac/ui/cs/advprog/product/service/CatalogueService.java
+++ src/main/java/id/ac/ui/cs/advprog/product/service/CatalogueService.java
@@ -3,9 +3,10 @@ package id.ac.ui.cs.advprog.product.service;
 import id.ac.ui.cs.advprog.product.model.Product;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface CatalogueService {
     List<Product> findAll();
-    Product findProductDetail();
-    List<Product> showFilteredProduct();
+    Product findProductDetail(UUID productID);
+    List<Product> showFilteredProduct(String filterType);
 }
--- src/main/java/id/ac/ui/cs/advprog/product/service/CatalogueServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/product/service/CatalogueServiceImpl.java
@@ -2,24 +2,37 @@ package id.ac.ui.cs.advprog.product.service;
 
 import id.ac.ui.cs.advprog.product.model.Product;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
+import id.ac.ui.cs.advprog.product.repository.ManageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+@Service
 public class CatalogueServiceImpl implements CatalogueService {
 
+    @Autowired
+    @Qualifier("productRepository")
+    ManageRepository<Product> repository;
     @Override
     public List<Product> findAll() {
+        Iterator<Product> productIterator = repository.findAll();
+        List<Product> products = new ArrayList<Product>();
+        productIterator.forEachRemaining(products::add);
+        return products;
+    }
+
+    @Override
+    public Product findProductDetail(UUID productID) {
         return null;
     }
 
     @Override
-    public Product findProductDetail() {
-        return null;
-    }
-
-    @Override
-    public List<Product> showFilteredProduct() {
+    public List<Product> showFilteredProduct(String filterType) {
         return null;
     }
 }
