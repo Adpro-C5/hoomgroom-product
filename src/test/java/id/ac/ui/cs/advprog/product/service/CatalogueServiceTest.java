63eef13001d8f0c3a64e38703f3ca6c618bfad6c
astrialyanda <alyandaastri19@gmail.com>
2024-05-11 11:58:14 +0700

    [RED] add tests for catalogue service

--- src/test/java/id/ac/ui/cs/advprog/product/service/CatalogueServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/product/service/CatalogueServiceTest.java
@@ -30,14 +30,20 @@ public class CatalogueServiceTest {
     void setUp() {
         Product product1 = new Product();
         UUID id = UUID.randomUUID();
+        ArrayList<String> productCategories1 = new ArrayList<>();
+        productCategories1.add("Kursi");
         product1.setId(id);
         product1.setProductName("Kursi Mewah");
+        product1.setCategories(productCategories1);
         product1.setSales(Integer.valueOf(1000));
 
         Product product2 = new Product();
         UUID id2 = UUID.randomUUID();
+        ArrayList<String> productCategories2 = new ArrayList<>();
+        productCategories2.add("Meja");
         product2.setId(id2);
         product2.setProductName("Meja Mewah");
+        product2.setCategories(productCategories2);
         product2.setSales(Integer.valueOf(500));
 
         products = new ArrayList<Product>();
@@ -57,11 +63,28 @@ public class CatalogueServiceTest {
 
     @Test
     void testFindProductDetail() {
-        
+        UUID productId = products.get(0).getId();
+
+        when(productRepository.findById(productId.toString())).thenReturn(products.get(0));
+
+        Product result = catalogueService.findProductDetail(productId);
+
+        verify(productRepository, times(1)).findById(productId.toString());
+        assertEquals(products.get(0), result);
     }
 
     @Test
     void testShowFilteredProduct() {
+        String filterType = "Kursi";
 
+        when(productRepository.findAll()).thenReturn(products.iterator());
+
+        List<Product> filteredProducts = catalogueService.showFilteredProduct(filterType);
+
+        verify(productRepository, times(1)).findAll();
+
+        assertEquals(1, filteredProducts.size());
+        assertEquals("Kursi Mewah", filteredProducts.get(0).getProductName());
+        assertEquals("Kursi", filteredProducts.get(0).getCategories().get(0));
     }
 }
