1678e48a1ebc663982a1059b9bcf71076aaef3eb
astrialyanda <alyandaastri19@gmail.com>
2024-05-14 17:18:37 +0700

    [GREEN] add catalogue controller

--- src/main/java/id/ac/ui/cs/advprog/product/controller/CatalogueController.java
+++ src/main/java/id/ac/ui/cs/advprog/product/controller/CatalogueController.java
@@ -1,16 +1,40 @@
 package id.ac.ui.cs.advprog.product.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import id.ac.ui.cs.advprog.product.model.Product;
+import id.ac.ui.cs.advprog.product.service.CatalogueService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class CatalogueController {
 
-    @GetMapping("/product/catalogue")
-    public String productCataloguePage() {
-        return "Catalogue";
+    @Autowired
+    CatalogueService catalogueService;
+
+    @PostMapping("/product/catalogue")
+    public ResponseEntity<List<Product>> productCataloguePage() {
+        List<Product> allProducts = catalogueService.findAll();
+        return new ResponseEntity<>(allProducts, HttpStatus.OK);
     }
 
-    @GetMapping("/product/details/")
-    public String productDetailPage() { return "Details"; }
+    @PostMapping("/product/details/{productId}")
+    public ResponseEntity<Product> productDetailPage(@PathVariable("productId") String productId) {
+        Product product = catalogueService.findProductDetail(productId);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/product/{filterType}/")
+    public ResponseEntity<List<Product>> productFilteredPage(@PathVariable("filterType") String filterType) {
+        List<Product> filteredProducts = catalogueService.showFilteredProduct(filterType);
+        return new ResponseEntity<>(filteredProducts, HttpStatus.OK); }
 }
