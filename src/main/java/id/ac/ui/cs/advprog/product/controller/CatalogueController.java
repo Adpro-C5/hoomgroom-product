e99df6ad66cc76ba7568cdce18c4cfac4c91b6eb
astrialyanda <alyandaastri19@gmail.com>
2024-05-30 21:13:36 +0700

    [REFACTOR] cross origin

--- src/main/java/id/ac/ui/cs/advprog/product/controller/CatalogueController.java
+++ src/main/java/id/ac/ui/cs/advprog/product/controller/CatalogueController.java
@@ -5,6 +5,7 @@ import id.ac.ui.cs.advprog.product.service.CatalogueService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
+@CrossOrigin
 @RestController
 public class CatalogueController {
 
@@ -42,7 +44,7 @@ public class CatalogueController {
     public ResponseEntity<List<Product>> productFilteredPage(@PathVariable("filterType") String filterType) {
         try {
             CompletableFuture<List<Product>> filteredProducts = catalogueService.showFilteredProduct(filterType);
-            if (filteredProducts != null) {
+            if (filteredProducts.get() != null) {
                 return new ResponseEntity<>(filteredProducts.get(), HttpStatus.OK);
             } else {
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
